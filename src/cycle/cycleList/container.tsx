82b480b0c1d3e35acc8debdb47b9eaff02b44f41
Hyjaz Loudin <hloudin@GENETEC.COM>
2019-02-12 17:50:00 -0500

    add liscycle list

--- src/cycle/cycleList/container.tsx
+++ src/cycle/cycleList/container.tsx
@@ -1,10 +1,36 @@
 import * as React from 'react'
 import * as fitness from 'fitness-goal-tracker'
 
+import { ListItem, ListItemText } from '@material-ui/core';
+
+import List from '@material-ui/core/List'
+
 class CycleList extends React.Component<fitness.User, any> {
-  render() {
+  render() : JSX.Element {
+    var cycles = this.props.cycles.map((cycle: fitness.Cycle, index: number) : JSX.Element => {
+      return (
+        <List>
+          <ListItem >
+            <ListItemText>
+              {cycle._id}
+            </ListItemText>
+          </ListItem>
+          <ListItem >
+            <ListItemText>
+              {cycle.startTime}
+            </ListItemText>
+          </ListItem>
+          <ListItem >
+            <ListItemText>
+              {cycle.endTime}
+            </ListItemText>
+          </ListItem>
+        </List>
+      )
+    })
     return (
       <div>
+        {cycles}
       </div>
     )
   }
